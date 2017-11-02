7a105b5c88492eefc11a5928470ef48531d848a6
imam <imam.asari@mplus.software>
2017-11-02 21:35:28 +0700

    udpate

--- application/views/footer_text.php
+++ application/views/footer_text.php
@@ -1,6 +1,6 @@
 <div class="footer">
 	<p style="">PT SUGAWARA KADII INDONESIA | Jl. raya Curug Kosambi Km 45, Cimahi Klari Karawang - Jawa Barat | Telp 0267-8642278 | Fax  0267-8642702 | info@sugawarkadii.co.id</p>
-	<p style="font-weight: bold; font-size: 15px; margin: 2px 0;">
+	<p style="font-weight: bold; font-size: 15px; margin: 3px 0;">
 		<a href="http://www.sugawarakogyo.co.jp" target="blank" >www.sugawarakogyo.co.jp</a> | 
 		<a href="http://www.kadii.co.id" target="blank" >www.kadii.co.id</a></p>
 	<p >copyright (c) 2017 PT. Sugawara Kadii Indonesia</p>
